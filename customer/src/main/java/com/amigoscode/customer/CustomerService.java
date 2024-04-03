45bb502078a55fe69c3b288853695be731efed05
Bee <huuphuoc252513@gmail.com>
2024-04-03 22:57:02 +1100

    EurekaServer added Fraud server added

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,16 +1,38 @@
 package com.amigoscode.customer;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-//        TODO: Save customer to the database
-        customerRepository.save(customer);
+        // check if fraudster
+//        customerRepository.save(customer); // this way we can not get the id of the customer
+        customerRepository.saveAndFlush(customer); // this way we can get the id of the customer
+
+        var fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("Customer is a fraudster");
+        }
+
+        // send notification
     }
+
+
 }
